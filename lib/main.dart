b111179f6995120ca7e01ac19c1d168b464d5867
Gandamu108 <105183098+Gandamu108@users.noreply.github.com>
2024-09-11 16:53:48 +0900

    最初のコミット

---
+++ lib/main.dart
@@ -0,0 +1,444 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'pdf.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ジーユー・ライフ 返品表',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 245, 230, 230)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: '返品表'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late String date; // lateキーワードを使って遅延初期化
+   List<String> title = ['日 付\nDate', '施設名\nFacility Name', 'フロア名\nFloor name', '記入者\nentrant'];
+   final List<Map<String, dynamic>> _checkedMaps = [
+    {'value': '上衣\njacket', 'checked': false},
+    {'value': '下衣\nLower clothing', 'checked': false},
+    {'value': '肌着上\nUnderwearTop', 'checked': false},
+    {'value': '肌着下\nunderwear', 'checked': false},
+    {'value': 'くつ下\nsocks', 'checked': false},
+    {'value': '小物\nAccessories', 'checked': false},
+    {'value': '寝具類\nBedding', 'checked': false},
+  ];
+   final List<Map<String, dynamic>> _checkedMapsf = [
+    {'value': '名前未記入 衣類に直接フルネームでご記入ください。\nName not written Please write your full name directly on the garment.', 'checked': false},
+    {'value': '名前が読み取れませんでした。再記入をお願いします\nName could not be read. Please retype it.', 'checked': false},
+    {'value': '以下のことが発生しています洗ってもよろしいでしょうか？\nThe following is happening', 'checked': false},
+  ];
+  final List<Map<String, dynamic>> _checkedRowRaps = [
+    {'value': '破れ\ntear', 'checked': false},
+    {'value': 'ほつれ\nfrayed spot', 'checked': false},
+    {'value': '縮み\nshrinkage', 'checked': false},
+    {'value': '変色\ndiscoloration', 'checked': false},
+    {'value': 'ボタンなし\nNo buttons', 'checked': false},
+  ];
+  final List<Map<String, dynamic>> _checkedMapsb = [
+    {'value': '契約外品目・未契約\nItems not contracted/not contracted', 'checked': false},
+    {'value': 'ドライ契約のないかたです。\nThis is the one without dry contract.', 'checked': false},
+    {'value': 'バーコード貼り付けできない衣類です\n(ダマール社製品・ポリプロピレン・ポリ塩化ビニル・ペットボトル再生系を含む衣類)\nClothing to which barcodes cannot be attached (Damar products, polypropylene, polyvinyl chloride, and clothing containing recycled plastic bottles)', 'checked': false},
+    {'value': '片足のため\nFor one leg', 'checked': false},
+    {'value': 'ドライ品になります。縮む可能性はありますが、水洗い\n処理をしてもよろしいでしょうか？\nIt will be dry goods. Is it possible to wash it in cold water?', 'checked': false},
+  ];
+
+  final TextEditingController _controllerFacilityName = TextEditingController();
+  final TextEditingController _controllerFloorName = TextEditingController();
+  final TextEditingController _controllerEntrant = TextEditingController();
+  final TextEditingController _controllerYourName = TextEditingController();
+  final TextEditingController _controllerOther = TextEditingController();
+
+  String _facilityName = '';
+  String _floorName = '';
+  String _entrant = '';
+  String _yourName = '';
+  String _other =  '';
+
+  @override
+  void initState() {
+    super.initState();
+    DateTime now = DateTime.now();
+    DateFormat outputFormat = DateFormat('MM/dd');
+    date = outputFormat.format(now); // initState内で日付をフォーマット
+  }
+
+  void dispose() {
+    _controllerFacilityName.dispose(); // メモリリークを防ぐためにdisposeする
+    _controllerFloorName.dispose();
+    _controllerEntrant.dispose();
+    super.dispose();
+  }
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    final List<Widget> titleWidgets = title.map((text) => Expanded(
+      child: Column(
+        children: [
+          _buildTextWithBorderText(context, text),
+          if (text == '日 付\nDate')
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(screenWidth * 0.003),
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color.fromARGB(255, 0, 0, 0))
+              ),
+              child: Text(
+                date,
+                style: TextStyle(fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          if (text == '施設名\nFacility Name')
+            TextFormField(
+              controller: _controllerFacilityName,
+              decoration: InputDecoration(
+                border: OutlineInputBorder()
+              ),
+            ),
+          if (text == 'フロア名\nFloor name')
+            TextFormField(
+              controller: _controllerFloorName,
+              decoration: InputDecoration(
+                border: OutlineInputBorder()
+              ),
+            ),
+          if (text == '記入者\nentrant')
+            TextFormField(
+              controller: _controllerEntrant,
+              decoration: InputDecoration(
+                border: OutlineInputBorder()
+              ),
+            )
+        ],
+      ),
+    )).toList();
+
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 248, 248, 248),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 10, left: screenWidth * 0.02, right: screenWidth * 0.02, bottom: 0),
+              width: double.infinity, // 画面サイズいっぱいまで広げる
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color.fromARGB(255, 0, 0, 0), width: 1),
+              ),
+              child: Text(
+                '返品連絡票 (バーコード用)\nReturn Contact Form (for barcode)',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 0, left: screenWidth * 0.02, right: screenWidth * 0.02, bottom: 0),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border(
+                  left: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                  right: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                ),
+              ),
+              child: Row(
+                children: titleWidgets,
+              ),
+            ),
+            Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 0, left: screenWidth * 0.02, right: screenWidth * 0.02, bottom: 0),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border(
+                      left: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                      right: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                    )
+                  ),
+                  padding: EdgeInsets.only(left: 15, right: 15),
+                  child: TextFormField(
+                    controller: _controllerYourName,
+                    decoration: InputDecoration(
+                      labelText: 'お名前  様 Your Name',
+                      labelStyle: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 0, left: screenWidth * 0.02, right: screenWidth * 0.02, bottom: 0),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border(
+                      left: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                      right: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                    )
+                  ),
+                  padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        '品物 goods',
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      SizedBox(width: 20,),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 0, left: screenWidth * 0.02, right: screenWidth * 0.02, bottom: 0),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      left: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                      right: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                    )
+                  ),
+                  padding: EdgeInsets.only(left: 15, right: 15),
+                  width: double.infinity,
+                  child: Row(
+                    children: _checkedMaps
+                      .map((q) => Flexible(
+                        child: CheckboxListTile(
+                        controlAffinity: ListTileControlAffinity.leading,
+                        title: Text(
+                          q['value'],
+                          style: TextStyle(fontSize: 25),
+                          ),
+                          value: q['checked'],
+                          onChanged: (bool? checkedValue) {
+                          setState(() {
+                            q['checked'] = checkedValue;
+                          });
+                        },
+                      ))
+                      )
+                    .toList()
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 0, left: screenWidth * 0.02, right: screenWidth * 0.02, bottom: 0),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      left: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                      right: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                    )
+                  ),
+                  padding: EdgeInsets.only(left: screenWidth * 0.10, right: screenWidth * 0.03),
+                  child: TextFormField(
+                    controller: _controllerOther,
+                    decoration: InputDecoration(
+                      labelText: 'その他 Other',
+                      labelStyle: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 0, left: screenWidth * 0.02, right: screenWidth * 0.02, bottom: 0),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border(
+                      left: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                      right: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                    )
+                  ),
+                  padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        '返却理由\nReason for return',
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ],
+                  ),
+                ),
+                // チェックボックスリストを表示
+                Container(
+                  margin: EdgeInsets.only(top: 0, left: screenWidth * 0.02, right: screenWidth * 0.02, bottom: 0),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      left: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                      right: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                    )
+                  ),
+                  padding: EdgeInsets.only(left: 15, right: 15),
+                  width: double.infinity,
+                  child: Column(
+                    children: _checkedMapsf
+                      .map((e) => CheckboxListTile(
+                        controlAffinity: ListTileControlAffinity.leading,
+                        title: Text(
+                          e['value'],
+                          style: TextStyle(fontSize: 25),
+                        ),
+                        // subtitle: Text(e['checked'] ? "ON" : "OFF"),
+                        value: e['checked'],
+                        onChanged: (bool? checkedValue) {
+                          setState(() {
+                            e['checked'] = checkedValue;
+                          });
+                        },
+                      ))
+                    .toList(),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 0, left: screenWidth * 0.02, right: screenWidth * 0.02, bottom: 0),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      left: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                      right: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                    )
+                  ),
+                  padding: EdgeInsets.only(left: 15, right: 15),
+                  width: double.infinity,
+                  child: Row(
+                    children: _checkedRowRaps
+                      .map((a) => Flexible(
+                        child: CheckboxListTile(
+                          controlAffinity: ListTileControlAffinity.leading,
+                          title: Text(
+                            a['value'],
+                            style: TextStyle(fontSize: 25),
+                          ),
+                          // subtitle: Text(a['checked'] ? 'ON' : 'OFF'),
+                          value: a['checked'],
+                          onChanged: (bool? checkedValue) {
+                            setState(() {
+                              a['checked'] = checkedValue;
+                            });
+                          },
+                        ),
+                      ))
+                      .toList(), // カンマを削除し、リストの生成が正しく終了するようにします。
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 0, left: screenWidth * 0.02, right: screenWidth * 0.02, bottom: 0),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      left: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                      right: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+                    )
+                  ),
+                  padding: EdgeInsets.only(top: 0 ,left: 15, right: 15, bottom: 0),
+                  width: double.infinity,
+                  child: Column(
+                    children: _checkedMapsb
+                      .map((b) => CheckboxListTile(
+                        controlAffinity: ListTileControlAffinity.leading,
+                        title: Text(
+                          b['value'],
+                          style: TextStyle(fontSize: 25),
+                        ),
+                        // subtitle: Text(b['checked'] ? 'ON': 'OFF'),
+                        value: b['checked'],
+                        onChanged: (bool? checkedValue) {
+                          setState(() {
+                            b['checked'] = checkedValue;
+                          });
+                        }
+                      ))
+                    .toList()
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 0, left: screenWidth * 0.02, right: screenWidth * 0.02, bottom: 0),
+                  height: 10,
+                  decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(color: const Color.fromARGB(255, 0, 0, 0))
+                    )
+                  )
+                ),
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              child: ElevatedButton(
+                onPressed: () async {
+                  _saveData();
+                  await generatePdf(
+                    date: date,
+                    facilityName: _facilityName,
+                    floorName: _floorName,
+                    entrant: _entrant,
+                    yourName: _yourName,
+                    other: _other,
+                    checkedMaps: _checkedMaps,
+                    checkedMapsf: _checkedMapsf,
+                    checkedRowRaps: _checkedRowRaps,
+                    checkedMapsb: _checkedMapsb,
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(110, 100),
+                ),
+                child: Text(
+                  '印刷\nPrinting',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextWithBorderText(BuildContext context, String text) {
+    double screenWidth = MediaQuery.of(context).size.width; // contextを使用して画面の幅を取得
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(screenWidth * 0.003),
+      decoration: BoxDecoration(
+        border: Border(
+          right: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+        ),
+      ),
+      child: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 25),
+      ),
+    );
+  }
+
+  void _saveData() async {
+    _facilityName = _controllerFacilityName.text;
+    _floorName = _controllerFloorName.text;
+    _entrant =  _controllerEntrant.text;
+    _yourName = _controllerYourName.text;
+    _other = _controllerOther.text;
+    print('施設名: $_facilityName\nフロア名: $_floorName\n記入者: $_entrant\nお名前: $_yourName\nその他: $_other');
+  }
+}
+
---
+++ lib/pdf.dart
@@ -0,0 +1,224 @@
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:printing/printing.dart';
+
+
+Future<void> generatePdf({
+  required String date,
+  required String facilityName,
+  required String floorName,
+  required String entrant,
+  required String yourName,
+  required String other,
+  required List<Map<String, dynamic>> checkedMaps,
+  required List<Map<String, dynamic>> checkedMapsf,
+  required List<Map<String, dynamic>> checkedRowRaps,
+  required List<Map<String, dynamic>> checkedMapsb,
+}) async {
+  final pdf = pw.Document();
+  // フォントの読み込み
+  final jpFont = pw.Font.ttf(await rootBundle.load('assets/fonts/NotoSansJP-VariableFont_wght.ttf'));
+
+  pdf.addPage(
+    pw.Page(
+      pageFormat: PdfPageFormat.a4.copyWith(marginTop: 0.05, marginLeft: 0.005, marginRight: 10, marginBottom: 0.005), // A4サイズで全体のマージンを設定
+      build: (context) {
+        return pw.Column(
+          crossAxisAlignment: pw.CrossAxisAlignment.start,
+          children: [
+            pw.Container(
+              width: 210, // 幅を全体に設定
+              decoration: pw.BoxDecoration(
+                border: pw.Border.all(),
+              ),
+              child: pw.Text(
+                '返品連絡票 (バーコード用)\nReturn Contact Form (for barcode)',
+                style: pw.TextStyle(fontSize: 13.5, fontWeight: pw.FontWeight.bold, font: jpFont),
+              ),
+            ),
+            pw.Container(
+              width: 215, // 幅を全体に設定
+              decoration: pw.BoxDecoration(border: pw.Border.all()),
+              child: pw.Row(
+                // mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                children: [
+                  pw.Text(
+                    '日付\nDate\n$date',
+                    style: pw.TextStyle(
+                      font: jpFont,
+                      fontSize: 8
+                      )
+                    ),
+                  pw.Text(
+                    '施設名\nFacility Name\n$facilityName',
+                    style: pw.TextStyle(
+                      font: jpFont,
+                      fontSize: 8
+                      ),
+                    ),
+                  pw.Text(
+                    'フロア名\nFloor Name\n$floorName',
+                    style: pw.TextStyle(
+                      font: jpFont,
+                      fontSize: 8
+                    )
+                    ),
+                  pw.Text(
+                    '記入者\nEntrant\n$entrant',
+                    style: pw.TextStyle(
+                      font: jpFont,
+                      fontSize: 8
+                    )
+                  )
+                  
+                ],
+              ),
+            ),
+            pw.Container(
+              alignment: pw.Alignment.center,
+              decoration: pw.BoxDecoration(
+                border: pw.Border(
+                  left: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0)),
+                  right: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0))
+                )
+                ),
+              child: pw.Row(
+              children: [
+                pw.Text(
+                  'お名前  様\nYour Name\n$yourName',
+                  style: pw.TextStyle(
+                    font: jpFont,
+                    fontSize: 10
+
+                  )
+                  ),
+              ],
+            ),
+            ),
+            pw.Container(
+              alignment: pw.Alignment.centerLeft,
+              decoration: pw.BoxDecoration(
+                border: pw.Border(
+                  left: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0)),
+                  right: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0))
+                )
+                ),
+              // padding: const pw.EdgeInsets.all(5),
+              child: pw.Text(
+                '品物',
+                style: pw.TextStyle(
+                  font: jpFont,
+                  fontSize: 10
+                )
+                )
+            ),
+            pw.Container(
+              alignment: pw.Alignment.centerLeft,
+              decoration: pw.BoxDecoration(
+                border: pw.Border(
+                  left: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0)),
+                  right: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0))
+                )
+                ),
+
+              child: pw.Text(
+                'その他(Other)\n$other',
+                style: pw.TextStyle(
+                  font: jpFont,
+                  fontSize: 10
+                )
+                )
+            ),
+            pw.Container(
+              alignment: pw.Alignment.centerLeft,
+              decoration: pw.BoxDecoration(
+                border: pw.Border(
+                  left: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0)),
+                  right: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0))
+                )
+                ),
+              // padding: const pw.EdgeInsets.all(5),
+              child: _buildCheckboxList(checkedMaps, jpFont)
+            ),
+            pw.Container(
+              alignment: pw.Alignment.centerLeft,
+              decoration: pw.BoxDecoration(
+                border: pw.Border(
+                  left: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0)),
+                  right: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0))
+                )
+                ),
+              // padding: const pw.EdgeInsets.all(5),
+              child: _buildCheckboxList(checkedMapsf, jpFont),
+            ),
+            pw.Container(
+              alignment: pw.Alignment.centerLeft,
+              decoration: pw.BoxDecoration(
+                border: pw.Border(
+                  left: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0)),
+                  right: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0))
+                )
+                ),
+              // padding: const pw.EdgeInsets.all(0),
+              child: _buildCheckboxList(checkedRowRaps, jpFont),
+            ),
+            pw.Container(
+              alignment: pw.Alignment.centerLeft,
+              decoration: pw.BoxDecoration(
+                border: pw.Border(
+                  left: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0)),
+                  right: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0)),
+                  bottom: pw.BorderSide(color: PdfColorCmyk.fromRgb(0, 0, 0))
+                )
+                ),
+              padding: pw.EdgeInsets.only(top: 10.0,left: 0.0,right: 10.0,bottom: 10.0),
+              child: pw.Wrap(
+                children: <pw.Widget>[
+
+                  _buildCheckboxList(checkedMapsb, jpFont)
+                ]
+              ),
+            ),
+          ],
+        );
+      },
+    ),
+  );
+
+  // PDFのプレビューと印刷
+  await Printing.layoutPdf(
+    onLayout: (PdfPageFormat format) async => pdf.save(),
+  );
+}
+
+pw.Widget _buildCheckboxList(List<Map<String, dynamic>> items, pw.Font fontSymbols) {
+  return pw.Container(
+    padding: pw.EdgeInsets.only(left: 5),
+    width: 200,
+    child: pw.Column(
+      children: [
+        pw.Wrap(
+          spacing: 8.0,
+          runSpacing: 4.0,
+          children: items
+              .map(
+                (item) => pw.Row(
+                  mainAxisSize: pw.MainAxisSize.max,
+                  children: [
+                    pw.Text(
+                      item['checked'] ? '✔' : '□', // Unicode チェックボックス
+                      style: pw.TextStyle(fontSize: 8, font: fontSymbols),
+                    ),
+                    // pw.SizedBox(width: 5),
+                    pw.Text(item['value'], style: pw.TextStyle(fontSize: 10, font: fontSymbols)),
+                  ],
+                ),
+              )
+              .toList(),
+        ),
+      ],
+    ),
+  );
+  
+}
